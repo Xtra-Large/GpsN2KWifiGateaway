2af9fd08d346fa05f5e622499c419edede710634
XtraLarge <80162630+Xtra-Large@users.noreply.github.com>
2024-11-25 13:30:08 +0100

    Update N2kDataToNMEA0183.h

--- N2kDataToNMEA0183.h
+++ N2kDataToNMEA0183.h
@@ -1,15 +1,18 @@
 /*
 N2kDataToNMEA0183.h
+
 Copyright (c) 2015-2018 Timo Lappalainen, Kave Oy, www.kave.fi
-Adding AIS (c) 2019 Ronnie Zeiller, www.zeiller.eu
+
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
 the Software without restriction, including without limitation the rights to use,
 copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
 Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
 PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
@@ -25,9 +28,9 @@ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 class tN2kDataToNMEA0183 : public tNMEA2000::tMsgHandler {
 public:
   using tSendNMEA0183MessageCallback=void (*)(const tNMEA0183Msg &NMEA0183Msg);
-
+    
 protected:
-  static const unsigned long RMCPeriod=500;
+  static const unsigned long RMCPeriod=1000;
   double Latitude;
   double Longitude;
   double Altitude;
@@ -42,7 +45,6 @@ protected:
   unsigned long LastPositionTime;
   unsigned long LastPosSend;
   unsigned long LastWindTime;
-  double RudderPosition;
   uint16_t DaysSince1970;
   double SecondsSinceMidnight;
   unsigned long NextRMCSend;
@@ -51,6 +53,7 @@ protected:
   tSendNMEA0183MessageCallback SendNMEA0183MessageCallback;
 
 protected:
+  void HandleSystemDateTime(const tN2kMsg& N2kMsg); // 126992
   void HandleHeading(const tN2kMsg &N2kMsg); // 127250
   void HandleVariation(const tN2kMsg &N2kMsg); // 127258
   void HandleBoatSpeed(const tN2kMsg &N2kMsg); // 128259
@@ -58,16 +61,7 @@ protected:
   void HandlePosition(const tN2kMsg &N2kMsg); // 129025
   void HandleCOGSOG(const tN2kMsg &N2kMsg); // 129026
   void HandleGNSS(const tN2kMsg &N2kMsg); // 129029
-  void HandleWind(const tN2kMsg &N2kMsg); //
-  void HandleRudder(const tN2kMsg &N2kMsg); // 127245
-
-  // AIS
-  void HandleAISClassAPosReport(const tN2kMsg &N2kMsg);   // 129038 AIS Class A Position Report
-  void HandleAISClassAMessage5(const tN2kMsg &N2kMsg);     // 129794 AIS class A Static and Voyage Related Data
-  void HandleAISClassBMessage18(const tN2kMsg &N2kMsg);    // 129039 AIS Class B Position Report
-  void HandleAISClassBMessage24A(const tN2kMsg &N2kMsg);  // 129809 AIS Class B "CS" Static Data Report, Part A
-  void HandleAISClassBMessage24B(const tN2kMsg &N2kMsg);  // 129810 AIS Class B "CS" Static Data Report, Part B
-
+  void HandleWind(const tN2kMsg &N2kMsg); // 130306
   void SetNextRMCSend() { NextRMCSend=millis()+RMCPeriod; }
   void SendRMC();
   void SendMessage(const tNMEA0183Msg &NMEA0183Msg);
